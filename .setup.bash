58d6a915c1f23c36cd63a8134a8443b9a1a342d6
Takayuki Murooka <takayuki5168@gmail.com>
2018-02-05 22:29:27 +0900

    first commit

---
+++ .setup.bash
@@ -0,0 +1,27 @@
+sudo apt-get install -y vim emacs24
+echo "alias emacs='emacs -nw \$2'" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install -y gnome-tweak-tool ibus-mozc
+
+sudo apt-get install -y cmake git clang-format-3.7
+sudo ln -s /usr/bin/clang-format-3.7 /usr/bin/clang-format
+sudo apt install -y libboost-all-dev
+sudo apt install -y python3-pip
+sudo pip3 install numpy scipy sklearn matplotlib
+git config --global user.name "Takayuki Murooka"
+git config --global user.email takayuki5168@gmail.com
+git config --global http.sslVerify false
+sudo apt install -y doxygen graphviz
+sudo apt install -y qt5-default
+sudo apt install -y gcc-arm-none-eabi
+
+sudo apt-get install -y zsh fish tmux
+
+sudo snap install slack --classic
+
+
+
+# その他
+# mozcの設定
+# chromium, Slack, LINEのインストール
