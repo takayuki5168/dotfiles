8286c89838f1ca94ab997a6659be464eb8a2eb9d
Takayuki Murooka <takayuki5168@gmail.com>
2018-05-01 11:38:22 +0900

    po

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,3 @@
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
-
-(load (concat user-emacs-directory "init-el-get.el"))
-
 (global-set-key "\C-h" 'delete-backward-char)
 (load-theme 'manoj-dark t)
 (setq backup-directory-alist '((".*"."~/.ehist")))
