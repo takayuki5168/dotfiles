0bc337a94158823b6aced90ddb2bd545fe09b8b1
Takayuki Murooka <takayuki5168@gmail.com>
2018-07-09 16:23:23 +0900

    po

--- setup.bash
+++ setup.bash
@@ -1,8 +1,8 @@
 sudo apt-get install -y vim emacs24
 echo "alias emacs='emacs -nw \$2'" >> ~/.bashrc
-source ~/.bashrc
+echo "alias suspend='systemctl suspend -i'" >> ~/.bashrc
 
-sudo apt-get install -y gnome-tweak-tool ibus-mozc curl
+sudo apt-get install -y gnome-tweak-tool curl
 
 sudo apt-get install -y cmake git clang-format-3.7
 sudo ln -s /usr/bin/clang-format-3.7 /usr/bin/clang-format
@@ -16,13 +16,20 @@ sudo apt install -y doxygen graphviz
 sudo apt install -y qt5-default
 sudo apt install -y gcc-arm-none-eabi
 
+sudo apt-get install ipython ipython3
+
 sudo apt-get install -y zsh fish tmux gnuplot
 
 sudo snap install slack --classic
 
+source ~/.bashrc
 
+# mozcの設定
+sudo apt-get install ibus-mozc
+killall ibus-daemon
+ibus-daemon -d -x &
+## 手動設定
 
 # その他
-# mozcの設定
 # Nodejsのインストール
 # chromium, Slack, LINEのインストール
