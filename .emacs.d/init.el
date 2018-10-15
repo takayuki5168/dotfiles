15a255f75557b28bf21dd3ce9d02da1633c0895f
Takayuki Murooka <takayuki5168@gmail.com>
2018-10-15 13:43:18 +0900

    delete clang-format and add emacs-trr

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,6 @@
 (global-set-key "\C-h" 'delete-backward-char)
 (load-theme 'manoj-dark t)
 (setq backup-directory-alist '((".*"."~/.ehist")))
+
+(add-to-list 'load-path "~/.emacs.d/.emacs-trr")
+(require 'trr)
